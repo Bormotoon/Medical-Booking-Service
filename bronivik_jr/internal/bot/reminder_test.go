6aaeb3d9ada8fb4c0bcc8f4783ae1fc852f8ba8b
Bormotoon <bormotoon@gmail.com>
2026-01-13 14:47:15 +0300

    refactor: enhance availability handling and validation logic

--- bronivik_jr/internal/bot/reminder_test.go
+++ bronivik_jr/internal/bot/reminder_test.go
@@ -258,8 +258,8 @@ func TestRateLimiterLogic(t *testing.T) {
 
 // Helper function
 func containsString(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(substr) == 0 ||
-		(len(s) > 0 && len(substr) > 0 && findSubstring(s, substr)))
+	return len(s) >= len(substr) && (s == substr || substr == "" ||
+		(s != "" && substr != "" && findSubstring(s, substr)))
 }
 
 func findSubstring(s, substr string) bool {
